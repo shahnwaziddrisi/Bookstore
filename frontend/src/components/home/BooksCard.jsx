dc5622832c052288b5b16293fad3e96028694fef
shahnwaziddrisi <shahnwaziddrisi@gmail.com>
2025-08-03 23:42:57 +0530

    Completed now(UI remaining)

---
+++ frontend/src/components/home/BooksCard.jsx
@@ -0,0 +1,13 @@
+import BookSingleCard from "./BookSingleCard";
+
+const BooksCard = ({ books }) => {
+  return (
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {books.map((items) => (
+        <BookSingleCard key={items._id} book={items} />
+      ))}
+    </div>
+  );
+};
+
+export default BooksCard;
--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Spinner from "../components/Spinner.jsx";
 import BackButton from "../components/BackButton.jsx";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const CreateBook = () => {
   const [title, setTitle] = useState("");
@@ -10,6 +11,7 @@ const CreateBook = () => {
   const [publishYear, setPublishYear] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
 
   const handleSaveBook = () => {
     const data = {
@@ -19,18 +21,61 @@ const CreateBook = () => {
     };
     setLoading(true);
     axios
-      .post("http://localhost:5555/books", data)
+      .post("http://localhost:5555/books/create", data)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar("Book Created Succesfully", { variant: "success" });
         navigate("/");
       })
       .catch((err) => {
-        console.log(err);
         setLoading(false);
+        enqueueSnackbar("Error creating book", { variant: "error" });
+        console.log(err);
       });
   };
 
-  return <div></div>;
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Create Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col border-2 border-sky-600 rounded-xl w-[600px] p-4 mx-auto">
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Author</label>
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Publish Year</label>
+          <input
+            type="text"
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+
+        <button className="p-2 bg-sky-300 m-8" onClick={handleSaveBook}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CreateBook;
